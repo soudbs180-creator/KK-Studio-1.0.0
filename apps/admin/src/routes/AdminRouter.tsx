f623160df3f911d3ac89335fc700251056641818
YYKK <soudbs180@gmail.com>
2026-04-11 01:38:29 +0800

    feat: add admin provider management page

--- apps/admin/src/routes/AdminRouter.tsx
+++ apps/admin/src/routes/AdminRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { AdminAuthProvider } from '../context/AdminAuthContext';
 import AdminShell from '../components/layout/AdminShell';
+import AdminProvidersPage from '../pages/AdminProvidersPage';
 import AdminDashboardPage from '../pages/AdminDashboardPage';
 import ExchangeRatesPage from '../pages/ExchangeRatesPage';
 import AdminLoginPage from '../pages/AdminLoginPage';
@@ -16,6 +17,7 @@ export default function AdminRouter() {
           <Route element={<AdminShell />}>
             <Route path="/" element={<AdminDashboardPage />} />
             <Route path="/exchange-rates" element={<ExchangeRatesPage />} />
+            <Route path="/providers" element={<AdminProvidersPage />} />
           </Route>
         </Route>
       </Routes>
